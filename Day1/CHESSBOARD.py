3ab2cf0f74dc93b627099bad1dde61a7e6690c98
SaiThrisha26 <160473865+SaiThrisha26@users.noreply.github.com>
2025-02-11 21:37:18 +0530

    CHESSBOARD.py

---
+++ Day1/CHESSBOARD.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+s=input()
+#a is odd 97,b=98 a1=odd,odd=b2=black,evenodd=oddeven=white
+# print(s[0])
+# print(s[1])
+if (ord(s[0])+int(s[1]))%2==0:
+  print("Black")
+else:
+  print("White")
+                   
